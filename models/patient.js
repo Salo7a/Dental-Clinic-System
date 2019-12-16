1a9991aba94acd801ef217236a47df8f5e3a7b3d
fadytadrous <43914477+fadytadrous@users.noreply.github.com>
2019-12-16 15:22:55 +0200

    Update patient.js

--- models/patient.js
+++ models/patient.js
@@ -1,46 +1,92 @@
-const Sequelize = require('sequelize');
-const sql = new Sequelize('test', 'root', null, {
-  dialect : 'mysql'
-});
-var patient  =sql.define('Patient',
-	{
-		name : Sequelize.STRING ,
-		ID : 
-			{ 
-			type : Sequelize.NUMBER ,
-			unique : true ,
-			allowNull : false,
-			primaryKey : true
-			},
-		Birthdate: Sequelize.DATE,
-		history : 
+
+module.exports = (sequelize, DataTypes) => {
+	const Patient = sequelize.define('Patient', {
+		Name: DataTypes.STRING,
+		NID: {
+			type: DataTypes.STRING
+			// unique: true,
+		},
+		Birthdate: DataTypes.DATE,
+		History :
 			{
-			type :Sequelize.STRING ,
-			defaultValue : 'no record available'
+				type :DataTypes.STRING ,
+				defaultValue : 'no record available'
 			},
-		Password : Sequelize.STRING(128),
-		Insurance : Sequelize.BOOLEAN ,
-		phone: Sequelize.NUMBER ,
-		Address : Sequelize.STRING,
-		Email : 
+		Password : DataTypes.STRING(128),
+		Insurance : DataTypes.BOOLEAN ,
+		Phone: DataTypes.STRING ,
+		Address : DataTypes.STRING,
+		Email :
 			{
-			type : Sequelize.STRING ,
-			isEmail : true ,
-			isNull : false
+				type : DataTypes.STRING ,
+				isEmail : true ,
+				isNull : false
 			}
+	}, {
+		classMethods: {
+			comparePassword: async function (Password, hash) {
+				// if bcrypt.compare() succeeds it'll call our function with
+				// (null, true), if password doesn't match it calls our function
+				// with (null, false), if it errors out it calls our function
+				// with (err, null)
+				await bcrypt.compare(Password, hash, function (err, isMatch) {
+					return isMatch;
+				});
+			}
+		},
 
-	}
-);
+	});
+	Patient.associate = function (models) {
+		// models.Doctor.hasMany(models.Patient);
+		// models.Doctor.hasMany(models.Appointments);
+		// models.Doctor.belongsTo(models.Department, {
+		//     onDelete: "CASCADE",
+		//     foreignKey: {
+		//         allowNull: false
+		//     }
+		// });
+	};
+	// This hook is called when an entry is being added to the back end.
+	// This method is used to hash the password before storing it
+	// in our database.
+
+	Patient.beforeCreate((Patient, options) => {
+		return bcrypt.genSalt(10).then(async salt => {
+			await bcrypt.hash(Patient.Password, salt).then(async hash => {
+					Patient.Password = hash;
+				}
+			).catch(err => {
+				throw new Error();
+			})
+		}).catch(err => {
+			throw new Error();
+		})
+
+	});
+	Patient.beforeUpdate((Patient, options) => {
+		return bcrypt.genSalt(10).then(async salt => {
+			await bcrypt.hash(Patient.Password, salt).then(async hash => {
+					Patient.Password = hash;
+				}
+			).catch(err => {
+				throw new Error();
+			})
+		}).catch(err => {
+			throw new Error();
+		})
+
+	});
+	Patient.prototype.comparePass = function (password) {
+		console.log("From Model: " + password);
+		return bcrypt.compareSync(password, this.Password);
+		//      .then(isMatch =>{
+		//     console.log(isMatch);
+		//     return isMatch;
+		// }).catch(err => {
+		//     throw new Error();})
+		//
+	};
 
 
-sql.sync();
-
-
-
-
-
-
-
-
-
-//module.exports = patient;
+	return Patient;
+};
