20ff9d42a551a6742147ac0e90b9078b846b4437
Salo7a <salo7a98x@gmail.com>
2019-12-16 03:31:31 +0200

    Comment Patient Model Till Fady Commits His New Changes

--- models/patient.js
+++ models/patient.js
@@ -1,46 +1,36 @@
-const Sequelize = require('sequelize');
-const sql = new Sequelize('test', 'root', null, {
-  dialect : 'mysql'
-});
-var patient  =sql.define('Patient',
-	{
-		name : Sequelize.STRING ,
-		ID : 
-			{ 
-			type : Sequelize.NUMBER ,
-			unique : true ,
-			allowNull : false,
-			primaryKey : true
-			},
-		Birthdate: Sequelize.DATE,
-		history : 
-			{
-			type :Sequelize.STRING ,
-			defaultValue : 'no record available'
-			},
-		Password : Sequelize.STRING(128),
-		Insurance : Sequelize.BOOLEAN ,
-		phone: Sequelize.NUMBER ,
-		Address : Sequelize.STRING,
-		Email : 
-			{
-			type : Sequelize.STRING ,
-			isEmail : true ,
-			isNull : false
-			}
-
-	}
-);
-
-
-sql.sync();
-
-
-
-
-
-
-
-
-
-//module.exports = patient;
+// const Sequelize = require('sequelize');
+// const sql = new Sequelize('test', 'root', null, {
+//     dialect : 'mysql'
+// });
+// var patient  =sql.define('Patient',
+//     {
+//         name : Sequelize.STRING ,
+//         ID :
+//             {
+//                 type : Sequelize.NUMBER ,
+//                 unique : true ,
+//                 allowNull : false,
+//                 primaryKey : true
+//             },
+//         Birthdate: Sequelize.DATE,
+//         history :
+//             {
+//                 type :Sequelize.STRING ,
+//                 defaultValue : 'no record available'
+//             },
+//         Password : Sequelize.STRING(128),
+//         Insurance : Sequelize.BOOLEAN ,
+//         phone: Sequelize.NUMBER ,
+//         Address : Sequelize.STRING,
+//         Email :
+//             {
+//                 type : Sequelize.STRING ,
+//                 isEmail : true ,
+//                 isNull : false
+//             }
+//
+//     }
+// );
+//
+//
+// sql.sync();
