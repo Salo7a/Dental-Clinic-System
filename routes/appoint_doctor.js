74baeca528ebea8b63e43e999ad0de22805e876e
shymaagamal <shymaagamal666@gmail.com>
2019-12-23 19:17:14 +0200

    Added Appointment Route

--- routes/appoint_doctor.js
+++ routes/appoint_doctor.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Doctor = require('../models').Doctor;
-const Medication = require('../models').Medication;
+const Appointment = require('../models').appointment;
 const Patient = require('../models').Patient;
 const {NotAuth, isAuth} = require('../utils/filters');
 const {check, validationResult, body} = require('express-validator');
@@ -10,10 +10,10 @@ const {Op} = require('sequelize');
 
 
 router.get('/viewappointment', function (req, res, next) {
-    Medication.findAll().then(function(appointment) {
+    Appointment.findAll().then(function(appointment) {
         res.render('appoint_doctor', {
             title: 'list of appointment',
-            Medications:appointment
+            appoint_doctor:appointment
         });
     });
 
