d73332d03817f2acce777b1b5dbb715ed27ca92f
NancySalah <nancysalah97@hotmail.com>
2019-12-24 00:03:44 +0200

    Added Patient History Route

--- routes/patientHistory.js
+++ routes/patientHistory.js
@@ -4,7 +4,7 @@ const histories = require('../models').patientHistory;
 
 
 router.get('/viewHistory', function (req, res, next) {
-    histories.findAll().then(function(history) {
+    histories.findAll().then(function (history) {
         res.render('viewHistory', {
             title: 'My History',
             patientHistory: history
