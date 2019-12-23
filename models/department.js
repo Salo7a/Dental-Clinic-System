f1934845fec764861cc762a78f2d2e00ec37a665
shymaagamal <shymaagamal666@gmail.com>
2019-12-24 00:15:15 +0200

    Editied Dep Model

--- models/department.js
+++ models/department.js
@@ -1,14 +1,24 @@
 'use strict';
+const models = require('../models');
+
 module.exports = (sequelize, DataTypes) => {
     const Department = sequelize.define('Department', {
         Name: DataTypes.STRING,
-        NID: DataTypes.STRING(14),
-        Number: DataTypes.INTEGER,
+        Head: DataTypes.INTEGER,
 
     }, {});
     Department.associate = function (models) {
          // Department.hasMany(models.Doctor);
-
+         Department.hasMany(models.Doctor, {
+            as: 'Doctor'
+        });
+        Department.hasOne(models.Doctor, {
+            foreignKey: 'Head',
+            targetKey: 'id'
+        });
+        // Department.hasMany(models.Patient, {
+        //     as: 'Patient'
+        // });
     };
     return Department;
 };
