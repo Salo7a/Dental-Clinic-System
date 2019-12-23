5d1eed5ce6c0bd2171e99fe9128104221db90625
shymaagamal <shymaagamal666@gmail.com>
2019-12-24 00:16:17 +0200

    Added Doctor Dep

---
+++ routes/admin_depart.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const router = express.Router();
+const Doctor = require('../models').Doctor;
+const Patient = require('../models').Patient;
+const {NotAuth, isAuth} = require('../utils/filters');
+const {check, validationResult, body} = require('express-validator');
+const {Op} = require('sequelize');
+const depart = require('../models').Department;
+router.get('/:id', function (req, res, next) {
+
+    depart.findAll().then(department => {
+
+        res.render('admin_depart',
+            {
+                title: 'department',
+                Department: department,
+                user: req.user
+            });
+
+    });
+
+});
+router.post('/:id', function (req, res, next) {
+    Appointment.findOne({
+        where:
+        {
+            Doctor_ID : req.params.id,
+            TIME: req.body.time
+        } 
+    }).then(appointment => {
+        if(!appointment)
+        {
+            Appointment.create({
+                Doctor_ID: req.params.id,
+                Patient_ID: req.user.id,
+                TIME:req.body.time,
+                Treat:req.body.treatment,
+                Notes :req.body.note
+
+            });
+           
+        }
+
+        res.render('appointment',
+            {
+                title: 'Appointment',
+                user: req.user
+            });
+
+    });
+});
+
+
+module.exports = router;
